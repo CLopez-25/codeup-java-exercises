6700298af40ecc2fca2d6af540c2a3321ea7563d
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-11 11:37:23 -0500

    finished java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,33 @@ public class HelloWorld {
         System.out.println(myFavoriteNumber);
         String myString = "string value";
         System.out.println(myString);
-        long myNumber;
+//        long myNumber = 123;
+        float myNumber = (float) 3.14;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+
+//        int x = 4;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        System.out.println(x * y);
+
+          int x = 10;
+          int y = 2;
+        System.out.println(x / y);
+        System.out.println(y - x);
     }
 }
