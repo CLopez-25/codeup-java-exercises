9e7124bf871b066683c370ca63a4a060da80d1ae
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-12 15:06:17 -0500

    started c s l lecture

---
+++ src/ControlStatementsLecture.java
@@ -0,0 +1,15 @@
+public class ControlStatementsLecture {
+    public static void main(String[] args) {
+//        Statements
+        int myInteger = 20;
+        int x = 6;
+        if (x == 5){
+            System.out.println("x is 5");
+        } else if (x > 5 && x < 15) {
+            System.out.println("x is between 5 and 15");
+        }
+
+
+
+    }
+}
