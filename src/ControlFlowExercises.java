6f0fd437137a71b66069349161d89bc490073754
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-13 13:33:39 -0500

    completed number 4 of exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -38,17 +38,38 @@ public class ControlFlowExercises {
 //         }
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What number would you like to go up to?");
-        boolean userWantsToContinue = false;
-        do {
-            int userInput = scanner.nextInt();
-            System.out.println("Here is your table!");
-            System.out.println("");
-            System.out.println("number | squared | cubed");
-            System.out.println("------ | ------- | -----");
-            for (int i = 1; i <= userInput; i++){
-                System.out.printf("%-7d| %-8d| %-5d%n", i, i * i, i * i * i);
-            }
-        } while (userWantsToContinue);
+//        System.out.println("What number would you like to go up to?");
+//        boolean userWantsToContinue = false;
+//        do {
+//            int userInput = scanner.nextInt();
+//            System.out.println("Here is your table!");
+//            scanner.nextLine();
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | ------- | -----");
+//            for (int i = 1; i <= userInput; i++){
+//                System.out.printf("%-7d| %-8d| %-5d%n", i, i * i, i * i * i);
+//            }
+//        } while (userWantsToContinue);
+
+                String userChoice = "yes";
+                while (!userChoice.equals("no")) {
+                    System.out.println("Enter the student's numerical grade: ");
+                    byte grade = scanner.nextByte();
+                    char letterGrade;
+                    if (grade > 0 && grade <= 59) {
+                        letterGrade = 'F';
+                    } else if (grade > 59 && grade <= 66) {
+                        letterGrade = 'D';
+                    } else if (grade > 66 && grade <= 79) {
+                        letterGrade = 'C';
+                    } else if (grade > 79 && grade <= 87) {
+                        letterGrade = 'B';
+                    } else {
+                        letterGrade = 'A';
+                    }
+                    System.out.println("The letter grade is: " + letterGrade);
+                    System.out.println("Would you like to enter another grade?(yes/no)");
+                    userChoice = scanner.next();
+                }
     }
 }
