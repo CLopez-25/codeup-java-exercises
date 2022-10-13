00581691363f68c490276e7ac47a8b8411bc949d
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-13 15:13:27 -0500

    refactored number four

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -33,43 +33,52 @@ public class ControlFlowExercises {
 //             System.out.println(i);
 //         }
 
+//         for (int i = 0; i <= 100; i += 2){
+//             System.out.println(i);
+//         }
+
+//         for (int i = 100; i >= -10; i -= 5){
+//             System.out.println(i);
+//         }
+
 //         for ( long i = 2; i <= 1000000; i*=i){
 //             System.out.println(i);
 //         }
 
         Scanner scanner = new Scanner(System.in);
-//        System.out.println("What number would you like to go up to?");
-//        boolean userWantsToContinue = false;
-//        do {
-//            int userInput = scanner.nextInt();
-//            System.out.println("Here is your table!");
-//            scanner.nextLine();
-//            System.out.println("number | squared | cubed");
-//            System.out.println("------ | ------- | -----");
-//            for (int i = 1; i <= userInput; i++){
-//                System.out.printf("%-7d| %-8d| %-5d%n", i, i * i, i * i * i);
-//            }
-//        } while (userWantsToContinue);
+        System.out.println("What number would you like to go up to?");
+        boolean userWantsToContinue = false;
+        do {
+            int userInput = scanner.nextInt();
+            System.out.println("Here is your table!");
+            scanner.nextLine();
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
+            for (int i = 1; i <= userInput; i++){
+//              left justified spaces in line below
+                System.out.printf("%-7d| %-8d| %-5d%n", i, i * i, i * i * i);
+            }
+        } while (userWantsToContinue);
 
-                String userChoice = "yes";
-                while (!userChoice.equals("no")) {
-                    System.out.println("Enter the student's numerical grade: ");
-                    byte grade = scanner.nextByte();
-                    char letterGrade;
-                    if (grade > 0 && grade <= 59) {
-                        letterGrade = 'F';
-                    } else if (grade > 59 && grade <= 66) {
-                        letterGrade = 'D';
-                    } else if (grade > 66 && grade <= 79) {
-                        letterGrade = 'C';
-                    } else if (grade > 79 && grade <= 87) {
-                        letterGrade = 'B';
-                    } else {
-                        letterGrade = 'A';
-                    }
-                    System.out.println("The letter grade is: " + letterGrade);
-                    System.out.println("Would you like to enter another grade?(yes/no)");
-                    userChoice = scanner.next();
-                }
-    }
-}
+        String userChoice = "yes";
+        while (!userChoice.equals("no")) {
+            System.out.println("Enter the student's numerical grade from 0 to 100: ");
+            byte grade = scanner.nextByte();
+            char letterGrade;
+            if ( grade <= 59) {
+                letterGrade = 'F';
+            } else if (grade <= 66) {
+                letterGrade = 'D';
+            } else if ( grade <= 79) {
+                letterGrade = 'C';
+            } else if ( grade <= 87) {
+                letterGrade = 'B';
+            } else {
+                letterGrade = 'A';
+            }
+            System.out.println("The letter grade is: " + letterGrade);
+            System.out.println("Would you like to enter another grade?(yes/no)");
+            userChoice = scanner.next();
+        }
+    } // end main
+} // end class
