e608d6f54c3333b3794019daf6c0cb5f5b19a54c
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-14 16:01:40 -0500

    completed 1a of methods exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,22 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+
+    }
+
+    public static void addition(int x, int y){
+        System.out.println( x + y);
+    }
+
+    public static void subtraction(int x, int y){
+        System.out.println( x - y);
+    }
+
+    public static void multiplication(int x, int y){
+        System.out.println( x * y);
+    }
+
+    public static void division(int x, int y){
+        System.out.println(x / y);
+    }
+
+}
