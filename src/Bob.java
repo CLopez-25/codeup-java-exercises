cef3468ffc9df8edc2c60657a94b00b94189f6ab
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-14 12:23:49 -0500

    completed bob

---
+++ src/Bob.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String response = "";
+
+        while (!response.equalsIgnoreCase("Quit")){
+            String userInput = scanner.nextLine();
+            if (userInput.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (userInput.endsWith("!")){
+                System.out.println("Whoa, chill out!");
+            } else if (userInput.equals("")){
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever");
+            }
+        }
+    } // main end
+} // class end
