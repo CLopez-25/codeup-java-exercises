49a514d5d62e4b59eaa66f1342f73561b410b2a9
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-14 13:19:30 -0500

    started methods lecture

---
+++ src/Dog.java
@@ -0,0 +1,13 @@
+public class Dog {
+    public static void bark(){
+        System.out.println("Woof Woof!");
+    }
+
+    public static void main(String[] args) {
+        bark();
+    }
+}
+
+// public class means we access this class from anywhere in our project
+// static means i dont have to create an object (dog)
+// void means it doesnt return anything
