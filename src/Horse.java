70491f197456e95fd712aca0eae80e640866a339
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-17 14:30:14 -0500

    completed javier's example

---
+++ src/Horse.java
@@ -0,0 +1,7 @@
+public class Horse {
+    public String name;
+    public String sound;
+    public void makeNoise(){
+        System.out.printf("%s goes %s", name, sound);
+    }
+}
---
+++ src/HorseTest.java
@@ -0,0 +1,8 @@
+public class HorseTest {
+    public static void main(String[] args) {
+        Horse horse1 = new Horse();
+        horse1.name = "Paloma";
+        horse1.sound = "Neeeeeeeigh!";
+        horse1.makeNoise();
+    }
+}
