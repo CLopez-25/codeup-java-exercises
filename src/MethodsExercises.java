f70f5e362824a42da1531bf54c6d36f44f0be104
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-17 12:24:03 -0500

    numbers 3 and 4 are completed

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -15,7 +15,7 @@ public class MethodsExercises {
 //        System.out.println( x * y);
 //    }
 //
-//    public static void division(int x, int y){
+//    public static double division(double int x, double int y){
 //        System.out.println( x / y);
 //    }
 //
@@ -36,27 +36,53 @@ public class MethodsExercises {
 //        }
 //    }
 
-    public static int getFactorial(){
+    public static int getFactorial() {
         int x, y, fact = 1;
+
+        System.out.printf("Give me a number between %d and %d\n", 1, 10);
         Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Enter a number from %d and %d: %n", 1, 10);
         x = scanner.nextInt();
 
         if (x < 0) {
-            System.out.println(" the number should not be negative.");
+            System.out.println("Number should be non-negative.");
             return x;
         } else {
             for (y = 1; y <= x; y++)
                 fact = fact * y;
-            System.out.println("The factorial of " + x + " is = " + fact);
+
+            System.out.println("Factorial of " + x + " is = " + fact);
             return x;
         }
-
     }
 
+    public static int generateRandomNumber(int max){
+        return (int) Math.round(Math.random() * max);
+    }
+
+    public static void diceRoll(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many sides do the dice have?");
+        byte numberOfSides = scanner.nextByte();
+        System.out.printf("Your dice have %d sides.%n", numberOfSides);
+        System.out.println("Would you like to roll the dice? y/n");
+        String userChoice = scanner.next();
+        while (userChoice.equals("y")){
+                System.out.println("You rolled the dice!");
+                System.out.printf("You rolled %d and %d%n", generateRandomNumber(numberOfSides), generateRandomNumber(numberOfSides));
+                System.out.println("Would like to roll the dice again?");
+                userChoice = scanner.next();
+            }
+        }
+
     public static void main(String[] args) {
+//        System.out.println(addition(5, 5));
+//        System.out.println(subtraction(10, 5));
+//        System.out.println(multiplication(2, 5));
+//        System.out.println(division(10, 2));
+//        System.out.println(modulus(20, 3));
 //        getInteger(1, 10);
-        getFactorial();
+//        getFactorial();
+        diceRoll();
     }
 }
