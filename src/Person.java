926f2bd098c9ead2c126ee7decb29065b4557d28
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-18 12:02:00 -0500

    number one fully complete

--- src/Person.java
+++ src/Person.java
@@ -11,5 +11,19 @@ public class Person {
         this.name = name;
     }
 
+    public void sayHello(){
+        System.out.println("Hello from " +this.name+ "!");
+    }
 
+    public Person(String name){
+        this.name = name;
+    }
+    public Person(){}
+
+
+    public static void main(String[] args) {
+        Person person1 = new Person("Cesar");
+        System.out.println(person1.getName());
+        person1.sayHello();
+    }
 }
