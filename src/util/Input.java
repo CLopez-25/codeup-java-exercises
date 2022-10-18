03c3714bb7cf07e37c06a0714f3756d1453ac00a
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-18 15:59:07 -0500

    almost finished with number 1

--- src/util/Input.java
+++ src/util/Input.java
@@ -17,6 +17,38 @@ public class Input {
         return scanner.nextLine();
     }
 
+    public boolean yesNo(){
+        System.out.println("Yes or No?");
+        String userChoice = this.scanner.nextLine();
+        return userChoice.equalsIgnoreCase("y") || userChoice.trim().equalsIgnoreCase("yes");
+    }
+
+    public int getInt(int min, int max){
+        System.out.printf("Enter a number between %d and %d\n", min, max);
+        int userNumber = scanner.nextInt();
+        if ((userNumber > max) && (userNumber < min)) {
+            System.out.println("Try again!");
+            return getInt(min, max);
+        } else {
+            System.out.printf("Your number was %d!%n", userNumber);
+            return scanner.nextInt();
+        }
+    }
+
+    public int getInt(){
+        return this.scanner.nextInt();
+    }
+
+//    public double getDouble(double min, double max) {
+//        double userNumber = scanner.nextInt();
+//        if ((userNumber > max) && (userNumber < min)) {
+//            System.out.println("Try again!");
+//            return getInt(double min, double max);
+//        } else {
+//            System.out.printf("Your number was %f!%n", userNumber);
+//            return scanner.nextDouble();
+//        }
+//    }
 
 
 
