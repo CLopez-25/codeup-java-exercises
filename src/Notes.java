6f1c34b59b5d6878095a46104c405129f07d4d17
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-18 09:53:54 -0500

    created a class for notes on java

---
+++ src/Notes.java
@@ -0,0 +1,40 @@
+public class Notes {
+}
+// public class means we access this class from anywhere in our project
+// static means i dont have to create an object (dog)
+// void means it doesnt return anything
+// when a constructor is called it creates an instance of the class
+
+// reference variables: objects:
+//Employee employee1 = new Employee(); // this is an instance of the class
+
+// a static method can be called on that class itself not an instance
+// the original string will stay stored in the systems memory, you are actually creating a new string it'll just reference to the original string and it'll evaluate to true
+
+// you lose data when you push past threshold of bytes
+// converting on data value to another = casting
+
+//    public static void main(String[] args) {
+//        // Declare a variable without initializing
+//        int weight;
+//        String breed;
+//        // Initializing after declaration
+//        weight = 12;
+//        breed = "Cavalier king charles spaniel";
+//        // initialize and declare all at the same time;
+//        String name = "Grinch";
+//        int cutenessFactor = 10;
+//        // Statement: "JVM, do something"
+//        System.out.println(name + " is a " + breed + ".");
+//        if (cutenessFactor > 8){
+//            System.out.println(name + " is cute.");
+//        } else {
+//            System.out.println(name + " needs to work on cuteness.");
+//        }
+//
+//        boolean isGrinchCute = true;
+//        char oneLetter = 'a';
+//        byte smallNumber; // bytes go from -128 to 127
+//        short meduimNumber; // shorts go from -32768 to 32767
+//        int number; // integers go c. -2 billion to 2 billion
+//        long bigNumber; // longs go about -9 quadrillion to 9 quadrillion
