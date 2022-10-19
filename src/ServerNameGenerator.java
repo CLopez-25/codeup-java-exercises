dce0b773b5e0612598e6ad66135428b58231e4ca
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-19 15:34:37 -0500

    completed number 2 of arrays exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,6 +1,20 @@
 package com.codeup.java;
 
+import java.util.Random;
+
 public class ServerNameGenerator {
     private static String[] adjectives = {"happy", "sad", "honest", "smart", "angry", "witty", "brave", "weird", "lovely", "dumb"};
     private static String[] nouns = {"ice", "bed", "rain", "car", "oil", "zoo", "dog", "boy", "cat", "pizza"};
+
+
+    public static String randomElement(String[] stringArray){
+        Random random = new Random();
+        int ranIndexOfArray = random.nextInt(stringArray.length);
+        return stringArray[ranIndexOfArray];
+    }
+
+
+    public static void main(String[] args) {
+        System.out.printf("Here is your server name:%n %s-%s",randomElement(adjectives), randomElement(nouns));
+    }
 }
