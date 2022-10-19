af51297dd152bf9dcadb0b3a6d913dd23dd39b4e
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-19 14:47:27 -0500

    started number 2 of exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -21,18 +21,16 @@ public class ArraysExercises {
         people[0] = new Person("Cesar");
         people[1] = new Person("Sky");
         people[2] = new Person("Oliver");
-        for (Person person : people){
-            System.out.println(person.getName());
-        }
-
-//        System.out.println("__________");
-
-        Person Paloma = new Person("Paloma");
-        people = addPerson(people, Paloma);
-        System.out.println(Paloma.getName());
 //        for (Person person : people){
 //            System.out.println(person.getName());
 //        }
 
+        System.out.println("__________");
+
+        Person Paloma = new Person("Paloma");
+        people = addPerson(people, Paloma);
+        for (Person person : people){
+            System.out.println(person.getName());
+        }
     }
 }
---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,6 @@
+package com.codeup.java;
+
+public class ServerNameGenerator {
+    private static String[] adjectives = {"happy", "sad", "honest", "smart", "angry", "witty", "brave", ""};
+    private static String[] nouns = {""};
+}
