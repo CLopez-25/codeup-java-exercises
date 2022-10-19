7f3297a5814859c15eda0456f9a0cf31348bb31c
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-19 15:00:28 -0500

    first part of number 2

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,6 +1,6 @@
 package com.codeup.java;
 
 public class ServerNameGenerator {
-    private static String[] adjectives = {"happy", "sad", "honest", "smart", "angry", "witty", "brave", ""};
-    private static String[] nouns = {""};
+    private static String[] adjectives = {"happy", "sad", "honest", "smart", "angry", "witty", "brave", "weird", "lovely", "dumb"};
+    private static String[] nouns = {"ice", "bed", "rain", "car", "oil", "zoo", "dog", "boy", "cat", "pizza"};
 }
