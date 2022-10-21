d0d45381f586e42afc2ec2d0e6e15955bafd5262
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-21 11:25:01 -0500

    working on shapes class exercise

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -1,5 +1,31 @@
 package com.codeup.java.Shapes;
 
 public class Rectangle {
+    private int width;
+    private int length;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Rectangle(){}
+
+    public Rectangle(int width, int length){
+        this.width = width;
+        this.length = length;
+    }
+
 
 }
---
+++ src/Shapes/ShapesTest.java
@@ -0,0 +1,7 @@
+package com.codeup.java.Shapes;
+
+public class ShapesTest {
+    public static void main(String[] args) {
+
+    }
+}
