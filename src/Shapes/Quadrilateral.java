4cd841d305baa2c8702d63c5899931f83b014f54
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-21 16:59:58 -0500

    almost done with number 3

--- src/Shapes/Quadrilateral.java
+++ src/Shapes/Quadrilateral.java
@@ -1,5 +1,17 @@
 package com.codeup.java.Shapes;
 
 public abstract class Quadrilateral extends Shape implements Measurable {
+    protected double width;
+    protected double length;
 
+    public Quadrilateral(double side, double side1){
+    super();
+    }
+
+    public void lengthWidth( double length, double width){
+        this.length = length;
+        this.width = width;
+
+
+    }
 }
