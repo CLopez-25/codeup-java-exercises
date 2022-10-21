031c284678be0afa7556004e8b8c339684fdcaae
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-21 11:44:03 -0500

    completed square class

--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -1,5 +1,18 @@
 package com.codeup.java.Shapes;
 
-public class Square {
+public class Square extends Rectangle {
+    public Square(){}
 
+    public int getArea(){
+        return (int) Math.pow(getLength(), 2);
+    }
+
+    public int getPerimeter(){
+        return 4 * getLength();
+    }
+
+    public Square(int side){
+        super.setLength(side);
+        super.setWidth(side);
+    }
 }
