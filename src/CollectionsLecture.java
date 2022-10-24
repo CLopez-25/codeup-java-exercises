71630d114b072c2c41e4636dd1d18c91f074fc95
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-24 13:49:41 -0500

    cllections lecture

--- src/CollectionsLecture.java
+++ src/CollectionsLecture.java
@@ -4,7 +4,7 @@ import com.codeup.java.util.ArrayList;
 
 public class CollectionsLecture {
     public static void main(String[] args) {
-        ArrayList<Monster> monsterArrayList = new ArrayList<>();
+//        ArrayList<Monster> monsterArrayList = new ArrayList<>();
     }
 }
 
@@ -12,3 +12,9 @@ public class CollectionsLecture {
 // armorclass, hitpoints, etc are all properties of the monster OBJECT.
 // if you have an array that isnt gonna change just let it be an array, if its an array that will change/ make it an arraylist.
 // jason's examples= 50 states wont change, if youre working for wayze the number of locations isnt gonna change so no need to use and array list.
+
+// use for loop in grocery list exercise per javier
+// you can have an arraylist of an arraylist
+// double colon gives you a reference to a method
+// DATA STRUCTURE = COLLECTIONS
+//
---
+++ src/HashMapsLecture.java
@@ -0,0 +1,10 @@
+package com.codeup.java;
+
+public class HashMapsLecture {
+    public static void main(String[] args) {
+
+    }
+}
+
+// think abput objects in javascript
+// data structure where a value is stored in a bucket referenced by a key in a string
