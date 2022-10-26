5d1aa359e6e2abcaa0cfac4824867bd183f587fd
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-25 19:42:07 -0500

    refactored grocerylistapp to not get lost in curlybrace land

--- src/groceries/GroceryListApp.java
+++ src/groceries/GroceryListApp.java
@@ -7,34 +7,52 @@ import java.util.Collections;
 import java.util.List;
 
 public class GroceryListApp {
-    //        method to generate an array list of grocery categories and sorting them using Collections.sort:
+//        method to generate an array list of grocery categories and sorting them using Collections.sort:
     public static ArrayList<String> generateCategories(){
         ArrayList<String> categories = new ArrayList<>(List.of(
                 "dairy",
                 "meat",
                 "fish",
-                "fruit",
-                "vegetable",
+                "fruits",
+                "vegetables",
                 "drinks",
                 "snacks",
-                "desert",
-                "grain",
-                "canned",
-                "frozen"
+                "desserts",
+                "grains",
+                "canned foods",
+                "frozen foods"
         ));
         Collections.sort(categories);
         return categories;
     }
+
+    public static void enterItem(Input input, ArrayList<String> categories){
+        System.out.print("Choose a category: ");
+//                starting traditional forLoop for the categories:
+        for (int i = 0; i < categories.size(); i++){
+            if (i == categories.size() -1){
+                System.out.printf("or %s.%n", categories.get(i));
+            } else {
+                System.out.printf("%s, ", categories.get(i));
+            }
+        } // end of loop over categories list
+    } // end of enterItem method
+
+    public static void generateGroceryList(Input input){
+//            reference to the arraylist once every time an item gets added:
+        ArrayList<String> categories = generateCategories();
+        boolean userWantsToEnterNewItem = input.yesNo("Would you like to enter a new item?");
+        if (userWantsToEnterNewItem){
+            enterItem(input, categories);
+        } // end of if (userWantsToEnterNewItem)
+    } // end of generateGroceryList method
+
     public static void main(String[] args) {
 //        beginning of prompts for the user using our overloaded boolean yesNo with a prompt
-
         Input input = new Input();
         boolean userChoice = input.yesNo("Would you like to create a grocery list? (yes/no)");
         if (userChoice){
-            boolean userWantsToEnterNewItem = input.yesNo("Would you like to enter a new item?");
-            if (userWantsToEnterNewItem){
-                System.out.println("logic for entering a new item");
-            }
-        }
-    }
-}
+            generateGroceryList(input);
+        } // end of if(userChoice)
+    } // end of main method
+} // end of groceryList app
