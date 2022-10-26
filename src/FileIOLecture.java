498970604eaab9e8aac20cb949fe06cef911b3ed
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-26 15:34:11 -0500

    updated fileio lecture

--- src/FileIOLecture.java
+++ src/FileIOLecture.java
@@ -1,7 +1,13 @@
 package com.codeup.java;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileIOLecture {
     public static void main(String[] args) {
@@ -20,17 +26,21 @@ public class FileIOLecture {
 //        System.out.println(fileLecturePath);
 //        System.out.println(fileLecturePath.toAbsolutePath());
 
-        Path otherPath = Paths.get("src", "../", "src", "../", "src", "fileIoLecture", "FileIOLecture");
-        System.out.println(otherPath);
-        System.out.println(otherPath.normalize());
-//        System.out.println(otherPath.toAbsolutePath());
-        System.out.println(otherPath.normalize().toAbsolutePath());
+//        Path otherPath = Paths.get("src", "../", "src", "../", "src", "fileIoLecture", "FileIOLecture");
+//        System.out.println(otherPath);
+//        System.out.println(otherPath.normalize());
+////        System.out.println(otherPath.toAbsolutePath());
+//        System.out.println(otherPath.normalize().toAbsolutePath());
 
         // ============================ FILE METHODS
 
+//        Path fileLecturePath = Paths.get("src", "../", "src", "../", "src", "fileIoLecture", "FileIOLecture");
+//        Path gitignoreFile = Paths.get(".gitignore");
 //        System.out.println(Files.exists(fileLecturePath));
 //        System.out.println(Files.exists(Paths.get("bob.txt")));
-//
+
+
+        // if a folder does not exist, this program will create it for you:
 //        String directory = "data";
 //        String filename = "info.txt";
 //        Path dataDirectory = Paths.get(directory);
@@ -43,10 +53,10 @@ public class FileIOLecture {
 //            try {
 //                Files.createDirectories(dataDirectory);
 //            } catch (IOException e) {
-//                e.printStackTrace();
+//                throw new RuntimeException(e);
 //            }
 //        }
-//
+////      you can also create files:
 //        if (Files.notExists(dataFile)) {
 //            try {
 //                Files.createFile(dataFile);
@@ -58,8 +68,8 @@ public class FileIOLecture {
 
         // ============================ READING FILES
 
-//        Path p = Paths.get("src", "weywot", "java", "test.txt");
-//
+//        Path p = Paths.get("src","test.txt");
+////
 //        List<String> lines = new ArrayList<>();
 //
 //        try {
@@ -67,7 +77,7 @@ public class FileIOLecture {
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
-//
+
 //        System.out.println(lines);
 //
 //        for (String name : lines) {
@@ -92,14 +102,15 @@ public class FileIOLecture {
 
         // ============================ WRITING FILES
 
-//        Path p = Paths.get("src", "weywot", "java", "test.txt");
+        // this adds the arrayList of names to the file selected:
+        Path p = Paths.get("src", "test.txt");
 
-//        List<String> newNames = Arrays.asList("John", "Fred", "Cathy");
-//        try {
-//            Files.write(p, newNames);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        List<String> newNames = Arrays.asList("John", "Fred", "Cathy");
+        try {
+            Files.write(p, newNames, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 //
 //        List<String> names = new ArrayList<>();
 //        names.add("Tim");
