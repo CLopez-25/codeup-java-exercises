5d03294420348725bdfce3623d70fb68aa66e991
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-01 15:43:37 -0500

    added very small example of recursion

--- src/Notes.java
+++ src/Notes.java
@@ -70,3 +70,10 @@ public class Notes {
 //}
 
 
+// 5! = 5 x 4 x 3 x 2 x 1 example for factorial
+// 5! = 5 x4!
+// thus n! = n x (n-1)!
+// hallmark 1: n! = n x(n-1)!
+//hallmark 2: 1! = 1, 2! = 2
+// note: its ok to just have 1! = 1 for the escape clause
+//
